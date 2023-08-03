ec922014586f74ba37c326fcda916899a3c399e5
Evgeny Arsaev <86007467+PeeWeeAnn@users.noreply.github.com>
2023-08-04 02:28:27 +0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path")
+const miniCss = require("mini-css-extract-plugin")
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [miniCss.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
+	plugins: [
+		new miniCss({
+			filename: "style.css",
+		}),
+	],
+}
