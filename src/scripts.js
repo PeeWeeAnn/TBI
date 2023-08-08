8d6e5f3b68664de960e0696aa3299ded0182e789
Evgeny Arsaev <vltby000@gmail.com>
2023-08-09 01:04:38 +0600

    Green burger

--- src/scripts.js
+++ src/scripts.js
@@ -6,11 +6,13 @@ const openModalNotes = document.getElementById("openModalNotes")
 const closeButtons = document.querySelectorAll(".modal-heading__close")
 
 // При нажатии на кнопки модалок, отобразить модальное окно
-openModalContent.onclick = function () {
-	modalContent.style.display = "block"
-}
-openModalNotes.onclick = function () {
-	modalNotes.style.display = "block"
+if(openModalContent && openModalNotes) {
+	openModalContent.onclick = function () {
+		modalContent.style.display = "block"
+	}
+	openModalNotes.onclick = function () {
+		modalNotes.style.display = "block"
+	}
 }
 
 // При нажатии на кнопки закрытия, скрыть модальное окно
@@ -18,5 +20,14 @@ for (let closeButton of closeButtons) {
 	closeButton.onclick = function () {
 		modalContent.style.display = "none"
 		modalNotes.style.display = "none"
+	}
+}
+
+const navBurger = document.querySelector('.burger-menu')
+const burgerButton = document.querySelectorAll('.header-blue-nav__burger-button')
+
+for(let button of burgerButton) {
+	button.onclick = function () {
+		navBurger.classList.toggle('burger-menu--active')
 	}
 }
